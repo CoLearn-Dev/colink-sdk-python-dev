e3a0e1457ed976aa688471437327fd8591decabb
quwenjie <2382036672@qq.com>
2022-08-28 10:47:09 +0800

    import new protos

--- colink/__init__.py
+++ colink/__init__.py
@@ -2,3 +2,7 @@ from colink.colink_pb2 import *
 from colink.colink_pb2_grpc import *
 from colink.colink_remote_storage_pb2 import *
 from colink.colink_remote_storage_pb2_grpc import *
+from colink.colink_policy_module_pb2 import *
+from colink.colink_policy_module_pb2_grpc import *
+from colink.colink_registry_pb2 import *
+from colink.colink_registry_pb2_grpc import *
