901fbab794ad1516c18dcb2fa13cfe9cc98729e1
quwenjie <2382036672@qq.com>
2022-11-04 17:43:54 +0800

    file end empty line+simpler var name for VT example

--- examples/protocol_test_variable_transfer.py
+++ examples/protocol_test_variable_transfer.py
@@ -10,13 +10,13 @@ pop = ProtocolOperator(__name__)
 @pop.handle("test_variable_transfer:initiator")
 def run_initiator(cl: CoLink, param: bytes, participants: List[CL.Participant]):
     logging.info("test_variable_transfer:initiator protocol operator!")
-    cl.set_variable("test:var_trans:output", param, participants[1:])
+    cl.set_variable("output", param, participants[1:])
 
 
 @pop.handle("test_variable_transfer:receiver")
 def run_receiver(cl: CoLink, param: bytes, participants: List[CL.Participant]):
     logging.info("test_variable_transfer:receiver protocol operator!")
-    receive_data = cl.get_variable("test:var_trans:output", participants[0])
+    receive_data = cl.get_variable("output", participants[0])
     cl.create_entry("tasks:{}:output".format(cl.get_task_id()), receive_data)
 
 
