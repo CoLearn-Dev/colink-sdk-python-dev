9a24113c5e5b273425a16690cb1d5462c84d9ac7
quwenjie <2382036672@qq.com>
2023-03-11 18:38:18 +0800

    change time 0.1

--- colink/protocol.py
+++ colink/protocol.py
@@ -100,7 +100,7 @@ class ProtocolOperator:
                 else:
                     err = q.get()
                     raise err
-                time.sleep(0.01)
+                time.sleep(0.1)
         else:
             counter = 0
             timer = time.time() + random.randint(32, 64)
@@ -127,7 +127,7 @@ class ProtocolOperator:
                     else:
                         counter = 0
                 # here we don't directly sleep 32~64s like rust because we have to detect sub-thread errors
-                time.sleep(0.01)
+                time.sleep(0.1)
 
     def run_attach(self, cl: CoLink):
         thread = Thread(
