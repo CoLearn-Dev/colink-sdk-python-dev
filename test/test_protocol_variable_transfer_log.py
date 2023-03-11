587d3d9076864a218f2ec24652296ede4815ebb6
quwenjie <2382036672@qq.com>
2023-03-11 22:20:16 +0800

    add more log

--- test/test_protocol_variable_transfer_log.py
+++ test/test_protocol_variable_transfer_log.py
@@ -4,12 +4,18 @@ import colink as CL
 import logging
 from colink import CoLink, byte_to_str, ProtocolOperator, InstantServer, InstantRegistry
 
+NUM=4
+
+def getid(cl:CoLink,participants: List[CL.Participant]):
+    for i in range(0,len(participants)):
+        if cl.get_user_id()==participants[i].user_id:
+            return i
 pop = ProtocolOperator(__name__)
 
 
 @pop.handle("variable_transfer_test:initiator")
 def run_initiator(cl: CoLink, param: bytes, participants: List[CL.Participant]):
-    for i in range(8):
+    for i in range(NUM):
         key = f"output{i}"
         key2 = f"output_remote_storage{i}"
         logging.warning(f"start send {i}")
@@ -23,23 +29,25 @@ def run_initiator(cl: CoLink, param: bytes, participants: List[CL.Participant]):
 
 @pop.handle("variable_transfer_test:receiver")
 def run_receiver(cl: CoLink, param: bytes, participants: List[CL.Participant]):
-    for i in range(8):
+    ID = getid(cl,participants)
+    for i in range(NUM):
         key = f"output{i}"
         key2 = f"output_remote_storage{i}"
-        logging.warning(f"start recv {i}")
+        logging.warning(f"{ID} start recv round {i}")
         msg = cl.recv_variable(key, participants[0])
         cl.create_entry(f"tasks:{cl.get_task_id()}:output{i}", msg)
-        logging.warning(f"end recv {i}")
+        logging.warning(f"{ID } end recv round {i}")
+        logging.warning(f"{ID} start recv storage round {i}")
         msg = cl.recv_variable_with_remote_storage(key2, participants[0])
         cl.create_entry(f"tasks:{cl.get_task_id()}:output_remote_storage{i}", msg)
-        logging.warning(f"end recv storage {i}")
+        logging.warning(f"{ID} end recv storage round {i}")
 
 
 def test_protocol_vt():
     ir = InstantRegistry()
     iss = []
     cls = []
-    for i in range(8):
+    for i in range(NUM):
         _is = InstantServer()
         cl = _is.get_colink().switch_to_generated_user()
         pop.run_attach(cl)
@@ -47,15 +55,15 @@ def test_protocol_vt():
         cls.append(cl)
 
     participants = [CL.Participant(user_id=cls[0].get_user_id(), role="initiator")]
-    for i in range(1, 8):
+    for i in range(1, NUM):
         participants.append(
             CL.Participant(user_id=cls[i].get_user_id(), role="receiver")
         )
     data = "test"
     task_id = cls[0].run_task("variable_transfer_test", data, participants, True)
 
-    for idx in range(1, 8):
-        for idx2 in range(0, 8):
+    for idx in range(1, NUM):
+        for idx2 in range(0, NUM):
             logging.warning(f"start read or wait user {idx} round{idx2}")
             res = cls[idx].read_or_wait(f"tasks:{task_id}:output{idx2}")
             logging.warning(f"end read or wait user {idx} round{idx2}")
